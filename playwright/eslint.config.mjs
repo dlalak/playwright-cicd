7838642363ef6dfd58c251311881e961cda623c5
Daniel Lalak <daniel.lalak@outlook.com>
2026-02-13 18:02:18 +0100

    Updated eslint files

--- playwright/eslint.config.mjs
+++ playwright/eslint.config.mjs
@@ -1,38 +1,34 @@
-import tsParser from '@typescript-eslint/parser';
-
 export default [
-    {
-      ignores: ['node_modules/', 'dist/', 'playwright-report/', 'test-results/'],
+  {
+    ignores: ['node_modules/', 'dist/', 'playwright-report/', 'test-results/'],
+  },
+  {
+    files: ['playwright/tests/**/*.ts'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
-    {
-      files: ['playwright/**/*.ts'],
-      languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
-      rules: {
-        'no-unused-vars': 'warn',
-        quotes: ['error', 'single', { allowTemplateLiterals: true }],
-        'no-restricted-properties': [
-          'error',
-          {
-            object: 'test',
-            property: 'only',
-            message: 'Do not commit focused tests. Remove test.only.',
-          },
-          {
-            object: 'describe',
-            property: 'only',
-            message: 'Do not commit focused suites. Remove describe.only.',
-          },
-          {
-            object: 'it',
-            property: 'only',
-            message: 'Do not commit focused tests. Remove it.only.',
-          },
-        ],
-      },
+    rules: {
+      'no-unused-vars': 'warn',
+      quotes: ['error', 'single', { allowTemplateLiterals: true }],
+      'no-restricted-properties': [
+        'error',
+        {
+          object: 'test',
+          property: 'only',
+          message: 'Do not commit focused tests. Remove test.only.',
+        },
+        {
+          object: 'describe',
+          property: 'only',
+          message: 'Do not commit focused suites. Remove describe.only.',
+        },
+        {
+          object: 'it',
+          property: 'only',
+          message: 'Do not commit focused tests. Remove it.only.',
+        },
+      ],
     },
-  ];
-  
+  },
+];
